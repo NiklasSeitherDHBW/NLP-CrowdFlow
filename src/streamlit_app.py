96ddb02017f758bbd82b9101a9485bd23c0f14b5
NiklasSeitherDHBW <s222792@student.dhbw-mannheim.de>
2024-12-22 21:40:08 +0100

    Added NLTK downloads

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -8,7 +8,7 @@ import yfinance as yf
 from plotly.subplots import make_subplots
 import os
 import urllib.request
-# import nltk
+import nltk
 
 CRYPTO_SYMBOLS = {"Bitcoin": "BTC-USD", "Ethereum": "ETH-USD", "Ripple": "XRP-USD"}
 SENTIMENTS = ["positive", "neutral", "negative"]
@@ -207,9 +207,9 @@ def main():
     apply_custom_styles()
     st.title("\U0001f4c8 Cryptocurrency Analysis Dashboard")
 
-    # nltk.download("punkt")
-    # nltk.download("stopwords")
-    # nltk.download("punkt_tab")
+    nltk.download("punkt")
+    nltk.download("stopwords")
+    nltk.download("punkt_tab")
 
     model = load_model()
 
