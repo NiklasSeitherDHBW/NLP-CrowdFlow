6829774a0a0fd01b07a8469258d532c29019a527
CarloRinderer <s223027@student.dhbw-mannheim.de>
2025-01-19 17:15:25 +0100

    Added missing packages

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -115,6 +115,8 @@ class NLTKTokenizer(BaseEstimator, TransformerMixin):
         nltk.download("punkt", quiet=True)
         nltk.download("stopwords", quiet=True)
         nltk.download("punkt_tab", quiet=True)
+        nltk.download("averaged_perceptron_tagger", quiet=True)
+        nltk.download('wordnet', quiet=True)
 
         self.stop_words = set(nltk.corpus.stopwords.words("english"))
         self.crypto_words = set()
